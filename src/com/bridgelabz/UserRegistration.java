b48692024f41e82120f63be5f0b5e5b96fc45f81
Annu <annusinghraj1998@gmail.com>
2022-06-29 22:38:46 +0530

    ADDED : Ability to check user's emailId valid or not

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -7,13 +7,18 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to user registration program");
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter first name");
         String firstName = scanner.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
 
         System.out.println("Enter last name");
         String lastName = scanner.next();
-        System.out.println(Pattern.matches("[A-Z][a-z]{2,}",lastName));
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
+
+        System.out.println("Enter emailId");
+        String emailId = scanner.next();
+        System.out.println(Pattern.matches("[a-z][A-Z a-z 0-9]+[@][a-z]+[.][a-z]{2,}", emailId));
 
     }
 
